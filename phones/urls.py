d474e6182ad16d31873ca5b71b3e40904cc2be0f
TarasovaYuliya <tarasovayuliya1.19@gmail.com>
2020-05-16 16:06:34 +0300

    Практическая работа №6

--- phones/urls.py
+++ phones/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name = 'phones'
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^register/$', views.RegisterFormView.as_view()),
+    url(r'^login/$', views.LoginFormView.as_view()),
+    url(r'^logout/$', views.LogoutView.as_view()),
+    url(r'^password-change/', views.PasswordChangeView.as_view()),
 ]
--- phones/views.py
+++ phones/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from .models import Phone
+# Базовый класс для обработки страниц с формами.
+from django.views.generic.edit import FormView
+# Спасибо django за готовую форму регистрации.
+from django.contrib.auth.forms import UserCreationForm
+# Спасибо django за готовую форму аутентификации.
+from django.contrib.auth.forms import AuthenticationForm
+# Функция для установки сессионного ключа.
+# По нему django будет определять,
+# выполнил ли вход пользователь.
+from django.contrib.auth import login
+# Для Log out с перенаправлением на главную
+from django.http import HttpResponseRedirect
+from django.views.generic.base import View
+from django.contrib.auth import logout
+# Для смены пароля - форма
+from django.contrib.auth.forms import PasswordChangeForm
 
 
 # главная страница
@@ -20,3 +36,81 @@ def index(request):
     )
 
 
+# базовый URL приложения, главной страницы -
+# часто нужен при указании путей переадресации
+app_url = "/phones/"
+
+
+# наше представление для регистрации
+class RegisterFormView(FormView):
+    # будем строить на основе
+    # встроенной в django формы регистрации
+    form_class = UserCreationForm
+    # Ссылка, на которую будет перенаправляться пользователь
+    # в случае успешной регистрации.
+    # В данном случае указана ссылка на
+    # страницу входа для зарегистрированных пользователей.
+    success_url = app_url + "login/"
+    # Шаблон, который будет использоваться
+    # при отображении представления.
+    template_name = "reg/register.html"
+
+    def form_valid(self, form):
+        # Создаём пользователя,
+        # если данные в форму были введены корректно.
+        form.save()
+        # Вызываем метод базового класса
+        return super(RegisterFormView, self).form_valid(form)
+
+
+# наше представление для входа
+class LoginFormView(FormView):
+    # будем строить на основе
+    # встроенной в django формы входа
+    form_class = AuthenticationForm
+    # Аналогично регистрации,
+    # только используем шаблон аутентификации.
+    template_name = "reg/login.html"
+    # В случае успеха перенаправим на главную.
+    success_url = app_url
+
+    def form_valid(self, form):
+        # Получаем объект пользователя
+        # на основе введённых в форму данных.
+        self.user = form.get_user()
+        # Выполняем аутентификацию пользователя.
+        login(self.request, self.user)
+        return super(LoginFormView, self).form_valid(form)
+
+
+# для выхода - миниатюрное представление без шаблона -
+# после выхода перенаправим на главную
+class LogoutView(View):
+    def get(self, request):
+        # Выполняем выход для пользователя,
+        # запросившего данное представление.
+        logout(request)
+        # После чего перенаправляем пользователя на
+        # главную страницу.
+        return HttpResponseRedirect(app_url)
+
+
+# наше представление для смены пароля
+class PasswordChangeView(FormView):
+    # будем строить на основе
+    # встроенной в django формы смены пароля
+    form_class = PasswordChangeForm
+    template_name = 'reg/password_change_form.html'
+    # после смены пароля нужно снова входить
+    success_url = app_url + 'login/'
+
+    def get_form_kwargs(self):
+        kwargs = super(PasswordChangeView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        if self.request.method == 'POST':
+            kwargs['data'] = self.request.POST
+        return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        return super(PasswordChangeView, self).form_valid(form)
